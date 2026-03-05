830dd7aee77fcdc5ae345d03db877f3c256127ce
Crowasang <bighyun620@naver.com>
2026-03-05 15:58:11 +0900

    Update PoolManager.cs

--- FruitProject/Assets/Script/PoolManager.cs
+++ FruitProject/Assets/Script/PoolManager.cs
@@ -6,7 +6,6 @@ public class PoolManager : MonoBehaviour
     public static PoolManager Instance;
 
     Dictionary<FruitData, Queue<GameObject>> _pools = new Dictionary<FruitData, Queue<GameObject>>();
-    Dictionary<GameObject, Queue<GameObject>> _audioPools = new Dictionary<GameObject, Queue<GameObject>>();
     
     private void Awake()
     {
@@ -53,36 +52,5 @@ public class PoolManager : MonoBehaviour
         
         obj.SetActive(false);
         _pools[data].Enqueue(obj);
-    }
-    
-    public GameObject Get(GameObject prefab, Vector3 pos, Quaternion rot)
-    {
-        // 해당 prefab이 큐에 있나?
-        if (!_audioPools.ContainsKey(prefab))
-        {
-            _audioPools.Add(prefab, new Queue<GameObject>());
-        }
-
-        GameObject obj;
-
-        if (_audioPools[prefab].Count > 0)
-        {
-            obj = _audioPools[prefab].Dequeue();
-        }
-        else
-        {
-            obj = Instantiate(prefab, transform);
-            obj.name = prefab.name;
-        }
-        obj.transform.position = pos;
-        obj.transform.rotation = rot;
-        obj.SetActive(true);
-
-        if (obj.TryGetComponent(out IPoolable poolable))
-        {
-            poolable.OnSpawn();
-        }
-        
-        return obj;
     }
 }
