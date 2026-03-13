85ea2bf9486664452e1451f7fcda2d660814b961
Crowasang <bighyun620@naver.com>
2026-03-13 14:13:23 +0900

    보는 이미지의 영상을 재생할지 애니메이션을 재생할지

--- XRStudy/Assets/ImageTracking/ImageTracker.cs
+++ XRStudy/Assets/ImageTracking/ImageTracker.cs
@@ -8,9 +8,7 @@ public class ImageTracker : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _prefabs;
     [SerializeField] private ARTrackedImageManager _arTrackedImageManager;
-    [SerializeField] private VideoPlayer _speaki;
     private Dictionary<string, GameObject> _spawnedObjects = new();
-    private Dictionary<string, VideoPlayer> _spawnObjects = new();
     private void OnEnable() 
         => _arTrackedImageManager.trackablesChanged.AddListener(OnTrackedImageChanged);
     private void OnDisable()
@@ -21,60 +19,18 @@ public class ImageTracker : MonoBehaviour
         // 이미지를 새로 인식했을 때
         foreach (ARTrackedImage trackedImage in args.added)
         {
-            // OnImageAdded(trackedImage);
-            OnVideoPlay(trackedImage);
+            OnImageAdded(trackedImage);
         }
         // 이미 인식된 이미지(트래킹중인) 업데이트할 때
         foreach (ARTrackedImage trackedImage in args.updated)
         {
-            // OnImageUpdated(trackedImage);
-            OnVideoUpdated(trackedImage);
+            OnImageUpdated(trackedImage);
         }
         // 트래킹에서 제거된 이미지 처리
         foreach (KeyValuePair<TrackableId, ARTrackedImage> removed in args.removed)
         {
-            // OnImageRemoved(removed.Value);
-            OnVideoRemoved(removed.Value);
+            OnImageRemoved(removed.Value);
         }
-    }
-
-    private void OnVideoPlay(ARTrackedImage trackedImage)
-    {
-        VideoPlayer spawnVideo = Instantiate(_speaki, trackedImage.transform.position, trackedImage.transform.rotation);
-        string imageName = trackedImage.referenceImage.name;
-        spawnVideo.transform.SetParent(trackedImage.transform);
-        spawnVideo.transform.localRotation = Quaternion.Euler(90, 0, 0);
-        _spawnObjects.Add(imageName, spawnVideo);
-    }
-    
-    private void OnVideoUpdated(ARTrackedImage trackedImage)
-    {
-        VideoPlayer spawnObject;
-        string imageName = trackedImage.referenceImage.name;
-        if (!_spawnObjects.TryGetValue(imageName, out spawnObject)) return;
-        // 현재 트래킹 상태에 따라
-        if (trackedImage.trackingState == TrackingState.Tracking)
-        {
-            spawnObject.Play();
-            spawnObject.transform.position = trackedImage.transform.position;
-            spawnObject.transform.rotation = trackedImage.transform.rotation;
-            spawnObject.transform.localRotation = Quaternion.Euler(90, 0, 0);
-        }
-        else
-        {
-            spawnObject.Pause();
-        }
-    }
-
-    private void OnVideoRemoved(ARTrackedImage trackedImage)
-    {
-        string imageName = trackedImage.referenceImage.name;
-        
-        VideoPlayer spawnedObject;
-        if (!_spawnObjects.TryGetValue(imageName, out spawnedObject)) return;
-        
-        Destroy(spawnedObject.gameObject);
-        _spawnObjects.Remove(imageName);
     }
     
     private void OnImageAdded(ARTrackedImage trackedImage)
@@ -94,14 +50,23 @@ public class ImageTracker : MonoBehaviour
 
         GameObject spawnedObject;
         if (!_spawnedObjects.TryGetValue(imageName, out spawnedObject)) return;
+        VideoPlayer video = spawnedObject.GetComponent<VideoPlayer>();
 
         // 현재 트래킹 상태에 따라
         if (trackedImage.trackingState == TrackingState.Tracking)
         {
-            spawnedObject.SetActive(true);
-            spawnedObject.transform.position = trackedImage.transform.position;
-            spawnedObject.transform.rotation = trackedImage.transform.rotation;
-            spawnedObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            if (video != null) 
+            {
+                spawnedObject.SetActive(true);
+                spawnedObject.transform.position = trackedImage.transform.position;
+                spawnedObject.transform.rotation = trackedImage.transform.rotation * Quaternion.Euler(90, 0, 0);
+            }
+            else 
+            {
+                spawnedObject.SetActive(true);
+                spawnedObject.transform.position = trackedImage.transform.position;
+                spawnedObject.transform.rotation = trackedImage.transform.rotation * Quaternion.Euler(0, 180, 0);
+            }
         }
         else
         {
